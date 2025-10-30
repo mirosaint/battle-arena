ba7959bc6e9eeded7e33991ff2b138fa8de8b0cc
Josale <ruslanmiroshnichenko2004@gmail.com>
2025-10-30 20:08:48 +0100

    Implement Attackable interface and abstract Character class

--- src/Attackable.java
+++ src/Attackable.java
@@ -1,3 +1,13 @@
 public interface Attackable {
+    /**
+     * Metoda odpowiadająca za zadanie obrażeń celowi.
+     * @param damage ilość obrażeń
+     */
+    void takeDamage(int damage);
 
+    /**
+     * Sprawdza, czy obiekt wciąż żyje (ma HP > 0).
+     * @return true jeśli żyje
+     */
+    boolean isAlive();
 }
--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,38 @@
-public class Character {
+public abstract class Character implements Attackable {
+
+    protected String name;
+    protected int health;
+    protected int attackPower;
+
+    public Character(String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    public abstract void attack(Attackable target);
+
+    @Override
+    public void takeDamage(int damage) {
+        health -= damage;
+        if (health < 0) health = 0;
+        System.out.println(name + " otrzymuje " + damage + " obrażeń! HP: " + health);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
 }
