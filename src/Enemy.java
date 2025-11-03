f357d2fe70146f88f05cddea5ef927fb8239aad1
Josale <ruslanmiroshnichenko2004@gmail.com>
2025-11-03 18:59:09 +0100

    Add Enemy class and simple battle loop (Warrior vs Enemy)

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,2 +1,18 @@
-public class Enemy {
+import java.util.Random;
+
+// ** Klasa reprezentująca przeciwnika.
+
+public class Enemy extends Character {
+
+    private static final Random random = new Random();
+
+    public Enemy(String name) {
+        super(name, 80 + random.nextInt(40), 10 + random.nextInt(10));
+    }
+
+    @Override
+    public void attack(Attackable target) {
+        System.out.println(name + " atakuje gracza!");
+        target.takeDamage(attackPower);
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,31 @@
-/**
- * Prosty test klasy Warrior
- */
+import java.util.Scanner;
+
 public class Game {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Warrior player = new Warrior("Gracz");
-        Warrior enemy = new Warrior("Przeciwnik");
+        Enemy enemy = new Enemy("Goblin");
 
-        System.out.println("=== Początek testowej walki ===");
+        System.out.println("=== Rozpoczyna się walka ===");
 
-        player.attack(enemy);
-        enemy.attack(player);
+        while (player.isAlive() && enemy.isAlive()) {
+            System.out.println("\nTwoja tura! Wciśnij ENTER, aby zaatakować.");
+            scanner.nextLine();
+            player.attack(enemy);
+
+            if (!enemy.isAlive()) {
+                System.out.println("Pokonałeś przeciwnika!");
+                break;
+            }
+
+            System.out.println("Tura przeciwnika!");
+            enemy.attack(player);
+
+            if (!player.isAlive()) {
+                System.out.println("Zostałeś pokonany!");
+            }
+        }
+
+        System.out.println("=== Koniec walki ===");
     }
 }
