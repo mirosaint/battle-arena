40620ae223eb8ee39d68098a84c5f4bb1fabbf95
Josale <ruslanmiroshnichenko2004@gmail.com>
2025-11-03 18:55:15 +0100

    Add Warrior class and simple console test in Game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,14 @@
+/**
+ * Prosty test klasy Warrior
+ */
 public class Game {
+    public static void main(String[] args) {
+        Warrior player = new Warrior("Gracz");
+        Warrior enemy = new Warrior("Przeciwnik");
+
+        System.out.println("=== Początek testowej walki ===");
+
+        player.attack(enemy);
+        enemy.attack(player);
+    }
 }
--- src/Warrior.java
+++ src/Warrior.java
@@ -1,2 +1,15 @@
-public class Warrior {
+/**
+ * Wojownik zadaje stałe obrażenia bazowe.
+ */
+public class Warrior extends Character {
+
+    public Warrior(String name) {
+        super(name, 100, 20);
+    }
+
+    @Override
+    public void attack(Attackable target) {
+        System.out.println(name + " uderza mieczem!");
+        target.takeDamage(attackPower);
+    }
 }
